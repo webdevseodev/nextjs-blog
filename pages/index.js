b7b79dacb187f8878c127a81929cafb873680c06
sahumo <7523460@gmail.com>
2022-08-16 04:12:49 +0300

    44

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Createeee Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
