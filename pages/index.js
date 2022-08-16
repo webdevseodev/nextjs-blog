1c21a23c59b0915659bd38bc29c5feca35c3bfa5
sahumo <7523460@gmail.com>
2022-08-16 04:02:22 +0300

    hgh

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link';
+import Image from 'next/image';
+
 
 export default function Home() {
   return (
@@ -17,8 +19,25 @@ export default function Home() {
       </a> */}
       this page!
     </Link>
+
+    
+   
+
+
+
+
 </h1>
-       
+<Image
+      src="/images/profile.jpg" // Route of the image file
+      height={144} // Desired size with correct aspect ratio
+      width={144} // Desired size with correct aspect ratio
+      alt="Your Name"
+    />
+ 
+
+
+
+
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,16 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
+      <Image
+      src="/images/profile.jpg" // Route of the image file
+      height={144} // Desired size with correct aspect ratio
+      width={144} // Desired size with correct aspect ratio
+      alt="Your Name"
+    />
       <h2>
         <Link href="/">
         {/* <a className="foo" target="_blank" rel="noopener noreferrer">
@@ -13,6 +20,18 @@ export default function FirstPost() {
             </Link>
         
       </h2>
+
+      
+    
+
+
+
+
     </>
+
+
+
+
+
   );
 }
